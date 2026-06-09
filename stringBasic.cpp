a5b228c864494723d56f8f9a5b0f119dc1d56114
rish3119 <rishavsingh312001@gmail.com>
2026-06-09 16:43:14 +0530

    Hello

--- stringBasic.cpp
+++ stringBasic.cpp
@@ -3,22 +3,7 @@
 using namespace std;
 
 int main() {
-	// Complete the program
-  string a;
-  string b; 
-  string c = a + b;
-     cin >> a >> b;
-   
-   cout << a.size()<< " " << b.size() << endl;
-   cout << a + b << endl;
-   string temp;
-   
-  /* temp[0]= a[0];
-   a[0] = b[0];
-   b[0] = temp[0];
-   
-   cout << a << " " << b << endl;*/
-   string a, b;
+    string a, b;
     cin >> a >> b;
 
     // 1. Print lengths
